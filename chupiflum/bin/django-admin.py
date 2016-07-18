f791a0ca1be5ca5dc1236ab15ff3cb227314e341
Ivan <trezeguet55@gmail.com>
2016-07-17 20:27:27 -0500

    Configuración del nuevo proyecto

---
+++ chupiflum/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ivan-dev/Escritorio/Developments/Refrescos/chupiflum/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
