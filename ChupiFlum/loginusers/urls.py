385f9fa3d469540cbd111c8303c70060066d28bb
ivan <trezeguet55@gmail.com>
2017-03-05 22:37:59 -0500

    <urls.py, loginuser> add route sigin

--- ChupiFlum/loginusers/urls.py
+++ ChupiFlum/loginusers/urls.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import views as auth_views
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import LoginUserApi
+
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'sigin', LoginUserApi, 'sigin')
 
 urlpatterns = [
-    url(r'^login/$', views.LoginView.as_view(), name = 'login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'),
-    url(r'^MenuPrincipal/$', views.menuView, name = 'menu'),
-    url(r'^$', views.homeView, name = 'home'),
+    url(r'^', include(router.urls)),
 ]
