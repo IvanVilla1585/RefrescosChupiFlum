7b3574329ee818051b256238f99e99086d8836b6
ivan <trezeguet55@gmail.com>
2017-03-05 22:40:33 -0500

    <views.py, permisos> add class getter permissions

--- ChupiFlum/permisos/views.py
+++ ChupiFlum/permisos/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .serializers import (
     ContentTypeSerializer,
     PermisosSerializer
 )
 from django.contrib.auth.models import Permission
+from django.contrib import admin
+admin.autodiscover()
+
+from rest_framework import permissions, routers, serializers, viewsets
+
+from oauth2_provider.ext.rest_framework import (TokenHasReadWriteScope, TokenHasScope, IsAuthenticatedOrTokenHasScope)
 
 class PermisosViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     model = Permission
     queryset = Permission.objects.all()
     serializer_class = PermisosSerializer
