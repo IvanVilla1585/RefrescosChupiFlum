8a1616a332bc78f13ecf505cfe37b88dfbd17162
ivan <trezeguet55@gmail.com>
2017-03-05 22:39:45 -0500

    <urls.py, permisos> add routes permisos

--- ChupiFlum/permisos/urls.py
+++ ChupiFlum/permisos/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from permisos.views import PermisosViewSet
+from rest_framework_jwt.views import obtain_jwt_token
 
 router = routers.DefaultRouter()
-router.register(r'permisos', PermisosViewSet)
+router.register(r'permisos', PermisosViewSet, base_name='permissions')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
