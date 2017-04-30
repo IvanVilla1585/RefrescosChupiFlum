be9ea273db6f79e3c03033555b8a59bf5a364451
Ivan Villa <trezeguet55@gmail.com>
2017-04-30 01:16:44 -0500

    <views.py> the views file are refactored to API

--- ChupiFlum/loginusers/views.py
+++ ChupiFlum/loginusers/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import viewsets
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from userprofiles.serializers import UserSerializer
 from django.contrib.auth.models import Permission
 from rest_framework.permissions import AllowAny
+from provider.utils import get_token_expiry
 
 
 class LoginUserApi(viewsets.ModelViewSet):
@@ -16,14 +18,16 @@ class LoginUserApi(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
     def create(self, request):
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+        objec = get_token_expiry(public=True)
+        print objec
         if username is None or username == '' or password is None or password == '':
-            return Response({'message': 'Ingrese los campos obligatorios'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Ingrese los campos obligatorios', 'status': 500}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             user = authenticate(username=username, password=password)
             if user is None:
-                return Response({'message': 'El usuario no existe'}, status=status.HTTP_200_OK)
+                return Response({'message': u'Usuario o contraseña incorrecto', 'status': 500}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             else:
                 serializer = UserSerializer(user)
-                return Response(serializer.data, status=status.HTTP_200_OK)
+                return Response({'user': serializer.data, 'status': 200}, status=status.HTTP_200_OK)
