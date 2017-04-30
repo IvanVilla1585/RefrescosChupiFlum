09b17c5579f5a2fb0c9e46ea4d51f64a52f470cb
Ivan Villa <trezeguet55@gmail.com>
2017-04-30 01:17:36 -0500

    <serializers.py> the serializer file is created

--- ChupiFlum/permisos/serializers.py
+++ ChupiFlum/permisos/serializers.py
@@ -7,10 +7,10 @@ from rest_framework import serializers
 class ContentTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContentType
-        fields = ('app_label', 'model',)
+        fields = ('id', 'app_label', 'model',)
 
 class PermisosSerializer(serializers.ModelSerializer):
-    content_type = ContentTypeSerializer()
+    content_type = ContentTypeSerializer(many=False)
     class Meta:
         model = Permission
-        fields = ('name', 'content_type', 'codename',)
+        fields = ('id', 'name', 'content_type', 'codename',)
