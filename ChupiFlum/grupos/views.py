b2b7f0f59e5b9ba0b4c10aecf4242d35bd83eb64
Ivan Villa <trezeguet55@gmail.com>
2017-04-30 01:16:09 -0500

    <views.py> the views file are refactored to API

--- ChupiFlum/grupos/views.py
+++ ChupiFlum/grupos/views.py
@@ -1,47 +1,20 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
-from django.template import loader
-from django.http import (
-    HttpResponse,
-    HttpResponseRedirect,
-    JsonResponse,
-)
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import (
-    CreateView,
-    UpdateView,
-    DeleteView
-)
-from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
+# -*- coding: utf-8 -*-
+from rest_framework.response import Response
+from rest_framework import status
 from django.contrib.auth.models import Group
-from loginusers.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
-from django.contrib.auth.decorators import login_required
-from .forms import GroupForm
+from .serializers import GroupSerializer
+from rest_framework import permissions, routers, serializers, viewsets
+from oauth2_provider.ext.rest_framework import (TokenHasReadWriteScope, TokenHasScope, IsAuthenticatedOrTokenHasScope)
 
-class CrearGrupo(CreateView):
+class GroupViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     model = Group
-    form_class = GroupForm
-    success_url = reverse_lazy('grupos:grupo')
+    queryset = model.objects.all()
+    serializer_class = GroupSerializer
 
-class GroupView(LoginRequiredMixin, TemplateView):
-    template_name = 'grupos/group_form.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(GroupView, self).get_context_data(**kwargs)
-        context.update({'form': GroupForm()})
-        return context
-
-
-@login_required()
-def grupoView(request):
-    form = GroupForm()
-    template = loader.get_template('grupos/group_form.html')
-    contex = {
-        'form': form
-    }
-    return HttpResponse(template.render(contex, request))
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
