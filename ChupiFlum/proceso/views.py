b84306a19439379aa8212cb000a60e530df2528b
Ivan Villa <trezeguet55@gmail.com>
2017-05-01 11:35:29 -0500

    <views.py> the functionalities of API are crated

--- ChupiFlum/proceso/views.py
+++ ChupiFlum/proceso/views.py
@@ -1,3 +1,62 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .serializer import ProcesoSerializer
+from .models import Proceso
+from django.contrib import admin
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
+from rest_framework.pagination import PageNumberPagination
+admin.autodiscover()
 
-# Create your views here.
+from rest_framework import permissions, routers, serializers, viewsets
+
+from oauth2_provider.ext.rest_framework import (TokenHasReadWriteScope, TokenHasScope, IsAuthenticatedOrTokenHasScope)
+
+class ProcesoViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    model = Proceso
+    queryset = model.objects.all()
+    serializer_class = ProcesoSerializer
+    pagination_class = PageNumberPagination
+
+    def get_queryset(self):
+        search = self.request.GET.get('search', None)
+        if search:
+            queryset = self.model.objects.filter(Q(nombre__icontains=search) | Q(maquina__name__icontains=search) )
+        else:
+            queryset = super(ProcesoViewSet, self).get_queryset()
+
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        request.data['maquina_id'] = request.data['maquina']
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        request.data['maquina_id'] = request.data['maquina']
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        id = self.kwargs[self.lookup_field]
+        process = None
+        if id:
+            try:
+                process = self.model.objects.get(id=id)
+                process.estado = not process.estado
+                process.save()
+            except Proceso.DoesNotExist as e:
+                return Response({'message': 'El proceso no existe'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'message': 'El id es obligatorio'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        serializer = self.get_serializer(process)
+        return Response(serializer.data, status=status.HTTP_200_OK)
