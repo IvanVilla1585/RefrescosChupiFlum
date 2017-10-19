320ed09072f8b2ad7ee655e8da0f6ccc14ccd414
Ivan Villa <trezeguet55@gmail.com>
2017-10-18 22:42:34 -0500

    remove unnecessary files

--- ChupiFlum/perfiles/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Perfil
-
-class PerfilesForm(forms.ModelForm):
-    class Meta:
-        model = Perfil
-        fields = '__all__'
--- ChupiFlum/perfiles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.listarPerfiles, name = 'heloword'),
-    url(r'^perfil/', views.perfil, name = 'perfil'),
-    url(r'^perfil/guardar', views.guardar, name = 'perfil'),
-]
