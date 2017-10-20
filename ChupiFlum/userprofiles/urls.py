f47c645c0101f18afbb535191f0fe7e20fabf112
Ivan Villa <trezeguet55@gmail.com>
2017-10-19 19:38:27 -0500

    <userprofiles urls.py>
    configurate routes

--- ChupiFlum/userprofiles/urls.py
+++ ChupiFlum/userprofiles/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+"""from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -8,4 +8,31 @@ urlpatterns = [
     url(r'^Usuarios/Consultar/(?P<id>[0-9]+)/$', views.ConsultarUsuario.as_view(), name='consultar'),
     ##url(r'^Usuarios/Eliminar/(?P<id>[0-9]+)$', views.EliminarUsuario.as_view(), name='eliminar'),
     url(r'^Usuarios/Listar', views.ListarUsuarios.as_view(), name='listar'),
+]
+"""
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+user_list = UserViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+user_active_account = UserViewSet.as_view({
+    'get': 'decode_token'
+})
+
+##router.register(r'users', UserViewSet, base_name='usuario')
+
+urlpatterns = [
+    url(r'^users/$', user_list, name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url(r'^users/account/$', user_active_account, name='decode-token'),
 ]
