8c18fb8a9d424a3993d3a9c8b4363efa695add12
Ivan Villa <trezeguet55@gmail.com>
2017-10-19 19:33:07 -0500

    <productos views.py>
    create functionalities

--- ChupiFlum/productos/views.py
+++ ChupiFlum/productos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+"""from django.shortcuts import render
 import json
 from django.core import serializers
 from django.http import (
@@ -216,3 +216,61 @@ class ProductoTerminadoView(LoginRequiredMixin, TemplateView):
         context.update({'form': ProductoTerminadoForm(), 'detalle_formula': detalle_formula })
 
         return context
+
+"""
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from .serializer import ProductoSerializer
+from .models import ProductoTerminado
+from django.contrib import admin
+from rest_framework.response import Response
+from rest_framework import status
+from django.db.models import Q
+from rest_framework.pagination import PageNumberPagination
+admin.autodiscover()
+
+from rest_framework import permissions, routers, serializers, viewsets
+
+from oauth2_provider.ext.rest_framework import (TokenHasReadWriteScope, TokenHasScope, IsAuthenticatedOrTokenHasScope)
+
+class ProveedoresViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    model = ProductoTerminado
+    queryset = model.objects.all()
+    serializer_class = ProductoSerializer
+
+    def get_queryset(self):
+        search = self.request.GET.get('search', None)
+        if search:
+            queryset = self.model.objects.filter(Q(nombre__icontains=search))
+        else:
+            queryset = super(ProveedoresViewSet, self).get_queryset()
+
+        return queryset
+
+    def destroy(self, request, *args, **kwargs):
+        id = self.kwargs[self.lookup_field]
+        proveedor = None
+        if id:
+            try:
+                proveedor = self.model.objects.get(id=id)
+                proveedor.estado = not proveedor.estado
+                proveedor.save()
+            except Proveedore.DoesNotExist as e:
+                return Response({'message': 'El proveedor no existe'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'message': 'El id es obligatorio'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        serializer = self.get_serializer(proveedor)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        typelist = self.request.GET.get('typelist', None)
+        queryset = self.filter_queryset(self.get_queryset())
+        print typelist
+        page = self.paginate_queryset(queryset)
+        if typelist is not None and page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
