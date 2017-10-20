a522e2c5272b84688ad6321d3d617f72ef680455
Ivan Villa <trezeguet55@gmail.com>
2017-10-19 19:34:25 -0500

    <proveedores urls.py>
    create routes

--- ChupiFlum/proveedores/urls.py
+++ ChupiFlum/proveedores/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import ProveedoresViewSet
+
+router = routers.DefaultRouter()
+router.register(r'proveedores', ProveedoresViewSet, base_name='proveedores')
 
 urlpatterns = [
-    url(r'^Proveedor/$', views.ProveedoresView.as_view(), name='proveedor'),
-    url(r'^Proveedor/Guardar/$', views.CrearProveedor.as_view(), name='crear'),
-    url(r'^Proveedor/Actualizar/(?P<id>[1-9]+)/$', views.ActualizarProveedor.as_view(), name='actualizar'),
-    url(r'^Proveedor/Consultar/(?P<id>[1-9]+)/$', views.ConsultarProveedor.as_view(), name='consultar'),
-    url(r'^Proveedor/Eliminar/$', views.ActualizarEstadoView.as_view(), name='eliminar'),
-    url(r'^Proveedor/Listar/$', views.ListarProveedores.as_view(), name='listar'),
-    url(r'^Proveedor/ReporteProveedoresPDF/$', views.ReporteProveedoresPDF.as_view(), name='reporte_pdf'),
+    url(r'^', include(router.urls)),
 ]
