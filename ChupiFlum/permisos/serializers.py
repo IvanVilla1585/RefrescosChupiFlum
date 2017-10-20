d262ab875167f44bcbf62a6a58b8f65ad6769f3e
Ivan Villa <trezeguet55@gmail.com>
2017-10-19 19:26:28 -0500

    <permisos serializer.py>
    refactor field

--- ChupiFlum/permisos/serializers.py
+++ ChupiFlum/permisos/serializers.py
@@ -11,6 +11,7 @@ class ContentTypeSerializer(serializers.ModelSerializer):
 
 class PermisosSerializer(serializers.ModelSerializer):
     content_type = ContentTypeSerializer(many=False)
+    ##content_type_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=ContentType.objects.all(), source='content_type')
     class Meta:
         model = Permission
         fields = ('id', 'name', 'content_type', 'codename',)
