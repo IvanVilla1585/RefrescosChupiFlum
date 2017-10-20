2ecc009a74a8bd6d214f271c2ea51d136e7bcd4f
Ivan Villa <trezeguet55@gmail.com>
2017-10-19 19:23:50 -0500

    <grupos views.py>
    refactor code

--- ChupiFlum/grupos/views.py
+++ ChupiFlum/grupos/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import Group
 from .serializers import GroupSerializer
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import permissions, routers, serializers, viewsets
 from oauth2_provider.ext.rest_framework import (TokenHasReadWriteScope, TokenHasScope, IsAuthenticatedOrTokenHasScope)
 
@@ -11,6 +12,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     model = Group
     queryset = model.objects.all()
     serializer_class = GroupSerializer
+    pagination_class = PageNumberPagination
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -18,3 +20,15 @@ class GroupViewSet(viewsets.ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def list(self, request, *args, **kwargs):
+        typelist = self.request.GET.get('typelist', None)
+        queryset = self.filter_queryset(self.get_queryset())
+        print typelist
+        page = self.paginate_queryset(queryset)
+        if typelist is not None and page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
